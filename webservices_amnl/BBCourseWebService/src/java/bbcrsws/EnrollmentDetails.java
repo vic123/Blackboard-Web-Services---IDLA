01c60a4cfa9c694a055de1533f3ace6f8fcfb8f9
vic123 <vic123@24d689cb-0286-47f4-9e54-8367b26d3f52>
2010-03-09 11:13:35 +0000

    Initial port of webservices/3) Beta/BBCourseWebService rev566

---
+++ webservices_amnl/BBCourseWebService/src/java/bbcrsws/EnrollmentDetails.java
@@ -0,0 +1,68 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+ */
+
+package bbcrsws;
+
+import blackboard.admin.data.course.Enrollment;
+
+/**
+ *
+ * @author Andrew.Martin@ncl.ac.uk
+ */
+public class EnrollmentDetails extends AbstractCourseDetails implements ReturnTypeInterface
+{
+    private String userRole;
+    
+    EnrollmentDetails(){}
+    EnrollmentDetails(Enrollment enr)
+    {
+        this.available = enr.getIsAvailable();
+        this.creationDate = extractDate(enr.getEnrollmentDate());
+        this.courseId = enr.getCourseSiteBatchUid();
+        this.userRole = enr.getRole().toFieldName();
+    }
+
+    public String getUserRole()
+    {
+	return this.userRole;
+    }
+
+    public void setUserRole(String userRole)
+    {
+	this.userRole = userRole;
+    }
+    
+    public String[] toStringArray()
+    {
+	return new String[]{this.courseId,
+			    Boolean.toString(this.available),
+			    this.creationDate,
+			    this.userRole
+	};
+    }
+
+    public String[] toStringArrayHeader()
+    {
+	return new String[]{"Course Id",
+			    "Available",
+			    "Date Created",
+			    "User Role"
+	};
+    }
+}
