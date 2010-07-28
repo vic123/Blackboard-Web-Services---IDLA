9406ba8fa3c1046e4ba9f79e53ba0861ab25638e
vic123 <vic123@24d689cb-0286-47f4-9e54-8367b26d3f52>
2010-07-28 19:07:09 +0000

    webservices_amnl/v2
    Initial port of v.1.2 based sources to webservices_amnl v2. Data Structures and  configuration files were copied/renamed without modifications and preserving of SVN history, 
    webservices_amnl\v2\src\java\bbws\BBWebService.java was initially copied from webservices_amnl\BBAnnouncementsWebService\src\java\bbaws\BBAnnouncementsWebService.java and then manually appended with code from BBCourseWebService, BBGradebookWebService, BBGroupWebService and BBUserWebService.

---
+++ trunk/webservices_amnl/v2/src/java/bbws/course/AbstractBBCourse.java
@@ -0,0 +1,75 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+ */
+
+package bbcrsws;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author Andrew.Martin@ncl.ac.uk
+ */
+public abstract class AbstractCourseDetails
+{
+    protected String courseId;
+    protected String creationDate;
+    protected Boolean available;
+    
+    protected String extractDate(Calendar cal)
+    {
+	try
+	{
+	    return cal.get(Calendar.YEAR)+"-"+(cal.get(Calendar.MONTH)+1)+"-"+cal.get(Calendar.DAY_OF_MONTH)+" "+cal.get(Calendar.HOUR_OF_DAY)+":"+cal.get(Calendar.MINUTE)+":"+cal.get(Calendar.SECOND);
+	}
+	catch(Exception e)
+	{
+	    return "Never";
+	}
+    }
+
+    public String getCourseId()
+    {
+	return this.courseId;
+    }
+
+    public void setCourseId(String courseId)
+    {
+	this.courseId = courseId;
+    }
+
+    public String getCreationDate()
+    {
+	return this.creationDate;
+    }
+
+    public void setCreationDate(String creationDate)
+    {
+	this.creationDate = creationDate;
+    }
+
+    public Boolean getAvailable()
+    {
+	return this.available;
+    }
+
+    public void setAvailable(Boolean available)
+    {
+	this.available = available;
+    }
+}
