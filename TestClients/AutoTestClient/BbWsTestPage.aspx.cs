76185f631574f7861cc8250c881daea31cd46ff3
vic123 <vic123@24d689cb-0286-47f4-9e54-8367b26d3f52>
2014-06-11 20:50:37 +0000

    AutoTestClient project type is converted from Web Site to a Web Application.

--- TestClients/AutoTestClient/BbWsTestPage.aspx.cs
+++ TestClients/AutoTestClient/BbWsTestPage.aspx.cs
@@ -10,10 +10,11 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 
 //http://connect.microsoft.com/VisualStudio/feedback/details/102416/partial-classes-do-not-work-in-asp-net-when-the-code-file-is-outside-of-the-app-code
-public partial class BbWsTestPage : BbWsTest
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        base.RunTest();
+namespace IDLA.WS.AutoTestClient {
+    public partial class BbWsTestPage : BbWsTest {
+        protected void Page_Load(object sender, EventArgs e) {
+            base.RunTest();
+        }
     }
+    
 }
--- TestClients/AutoTestClient/Default.aspx.cs
+++ TestClients/AutoTestClient/Default.aspx.cs
@@ -1,18 +1,20 @@
-using System;
-using System.Data;
-using System.Configuration;
+﻿using System;
 using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
 
-public partial class _Default : System.Web.UI.Page
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
+namespace AutoTestClient {
+    public partial class _Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
 
+        }
     }
 }
